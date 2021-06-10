f53f79d6c6a6b4ff1391b0ce6047a8ee98937d61
Md.Abu Naim <78495282+Abunaim3031@users.noreply.github.com>
2021-06-10 06:22:44 +0600

    File Add

---
+++ js/index.js
@@ -0,0 +1,52 @@
+let correct_ans = getRandomIntInclusive(1, 10);
+
+let Result;
+
+//define function
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+  //The maximum is inclusive and the minimum is inclusive
+}
+
+
+for (var j = 1; j <= 3; j++) {
+  let input1 = prompt("Guess your Correct Number(1-10)");
+
+  input1 = parseInt(input1);
+
+  if (input1 == correct_ans) {
+
+    Result = "You Win!"
+  }
+
+  else if (input1 > correct_ans) {
+
+    Result = "Correct Answer is smaller!"
+  }
+
+  else if (input1 < correct_ans) {
+
+    Result = "Correct Answer is Grater!"
+
+  }
+
+
+  alert(Result)
+
+  if (Result == "You Win!") {
+    break;
+  }
+
+}
+
+
+
+if (Result != "You Win!") {
+
+  alert("You lost!");
+
+}
+
